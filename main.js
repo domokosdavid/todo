28b24743834ad3d6579fad983e8f98a75238a553
Your Name <you@example.com>
2021-10-13 17:28:49 +0200

    javítások, áthúzás még mindig nem működik

--- main.js
+++ main.js
@@ -1,30 +1,28 @@
 function general(){
     let adat = document.getElementById("szoveg").value;
     let li = document.createElement("li");
+    if (adat < 1){
+        alert("Az input mező nem lehet üres!");
+        return;
+    }
     li.setAttribute("class","list-group-item");
     let check = document.createElement("input");
-    check.addEventListener("change",athuz);
+    check.addEventListener("change",athuz)
     check.setAttribute("type","checkbox");
-    
     let x = document.createElement("span");
-    x.style.marginLeft="200px";
-    
-    
+    x.style.marginLeft="20%"; 
     x.innerHTML="X";
     x.addEventListener("click", torol);
     li.appendChild(check);
     
     li.innerHTML += adat;
     li.appendChild(x);
-    if (adat < 1){
-        alert("Az input mező nem lehet üres!");
-    }
-    else{
+    
+
         
         document.getElementById("lista").appendChild(li);
         
         document.getElementById("szoveg").value = "";
-    }
     
     
 }
@@ -34,15 +32,15 @@ function torol(e){
 function athuz(e){
     if(e.target.checked){
         e.target.parentElement.style.textDecoration="line-through";
-        e.target.parentElement.setAttribute("class","list-group-item")
-    }
-    else{
-        e.target.parentElement.style.textDecoration="none";
-        e.target.parentElement.setAttribute("class","list-group-item")
+        e.target.parentElement.setAttribute("class","list-group-item elvegzett");
+    }else{
+        e.target.parentElement.style.textDecoration="none"
+        e.target.parentElement.setAttribute("class","list-group-item");
     }
 }
 
 function init(){
     document.getElementById("felveszgomb").addEventListener("click", general);
+    
 }
 document.addEventListener("DOMContentLoaded",init);
